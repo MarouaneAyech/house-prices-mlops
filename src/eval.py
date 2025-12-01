e65842af1c5d3bfd6896804f0ba91a5fc9212db6
Marouane <marouane.benhaj.ayech.2020@gmail.com>
2025-12-01 17:48:06 +0100

    pipeline defini

--- src/eval.py
+++ src/eval.py
@@ -2,8 +2,11 @@ import pandas as pd
 import pickle
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import numpy as np
+import os
 
-def evaluate_model(data_dir="data/processed", model_path="models/model.pkl"):
+def evaluate_model(data_dir="data/processed", model_path="models/model.pkl", output_dir='metrics'):
+    os.makedirs(output_dir, exist_ok=True)
+    
     X_test = pd.read_csv(f"{data_dir}/X_test.csv")
     y_test = pd.read_csv(f"{data_dir}/y_test.csv")
     X_train = pd.read_csv(f"{data_dir}/X_train.csv")
@@ -23,6 +26,13 @@ def evaluate_model(data_dir="data/processed", model_path="models/model.pkl"):
     r2 = r2_score(y_test, y_test_pred)
     print(f"Test RMSE: {rmse:.2f}")
     print(f"Test R²: {r2:.3f}")
+
+    
+    with open(f"{output_dir}/metrics.txt","w") as file :
+        file.write(f"Train RMSE: {rmse:.2f}\n")
+        file.write(f"Train R²: {r2:.3f}\n")
+        file.write(f"Test RMSE: {rmse:.2f}\n")
+        file.write(f"Test R²: {r2:.3f}\n")
 
 if __name__ == "__main__":
     evaluate_model()
